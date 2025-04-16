1297515371f5b49848a377df4b7ee35e5c4b2035
Victor Rodriguez <awtGerry@gmail.com>
2025-04-16 16:40:53 -0600

    update: Importar, exportar y cerrar funcionando
    Hay muchos warnings en la terminal, se arreglaran en su momento.

--- src/lib/utilities/fileHandler.ts
+++ src/lib/utilities/fileHandler.ts
@@ -1,3 +1,8 @@
+import { loadAssignments } from "$lib/modules/entities/assignments";
+import { loadClassrooms } from "$lib/modules/entities/classroomStore";
+import { loadGroups } from "$lib/modules/entities/groupsStore";
+import { loadSubjects, loadSubjectsWithTeachers } from "$lib/modules/entities/subjectsStore";
+import { loadTeachers } from "$lib/modules/entities/teachersStore";
 import { invoke } from "@tauri-apps/api"; 
 
 export async function exportFile(): Promise<void> {
@@ -6,4 +11,20 @@ export async function exportFile(): Promise<void> {
 
 export async function importFile(): Promise<void> {
   await invoke("import_file");
+  await loadTeachers();
+  await loadSubjects();
+  await loadSubjectsWithTeachers();
+  await loadGroups();
+  await loadClassrooms();
+  await loadAssignments();
+}
+
+export async function deleteAll(): Promise<void> {
+  await invoke("delete_all_data");
+  await loadTeachers();
+  await loadSubjects();
+  await loadSubjectsWithTeachers();
+  await loadGroups();
+  await loadClassrooms();
+  await loadAssignments();
 }
