9ae65fd0800fb002cf977d4de083b82b37444412
Victor Rodriguez <awtGerry@gmail.com>
2025-04-22 12:43:49 -0600

    update: Intenta abrir el modelo desde js

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+
+export default defineConfig({
+  plugins: [sveltekit()],
+  optimizeDeps: {
+    exclude: ['@xenova/transformers']
+  },
+  server: {
+    headers: {
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Opener-Policy': 'same-origin',
+    }
+  }
+}); 
